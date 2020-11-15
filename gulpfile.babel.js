d45c231e4b1847cde82deb8a29785d4fd234045f
Eduardo Tell <eduardo-tell@hotmail.com>
2020-11-15 20:15:29 -0300

    Varias correções de layout

---
+++ gulpfile.babel.js
@@ -0,0 +1,31 @@
+import gulp from 'gulp';
+import sass from 'gulp-sass';
+import concat from 'gulp-concat';
+import browserSync from "browser-sync";
+
+exports.sass = () => (
+    gulp.src('./assets/scss/**/**')
+    .pipe(sass({outputStyle: 'compressed', errLogToConsole: true }))
+    .pipe(concat('global.min.css'))
+    .pipe(gulp.dest('./dist/css'))
+);
+
+gulp.task('watch', () => {
+    gulp.watch('.dist/css/**', gulp.series('sass'))
+});
+
+gulp.task('serve', () => {
+    browserSync.init({
+        server: {
+            baseDir: './',
+            index: 'index.html'
+        },
+        notify: false,
+        injectChanges: true
+    });
+
+    gulp.watch('./assets/scss/**', gulp.series('sass'))
+    gulp.watch('./**/**/**').on('change', browserSync.reload);
+});
+
+gulp.task('default', gulp.series('watch'));
